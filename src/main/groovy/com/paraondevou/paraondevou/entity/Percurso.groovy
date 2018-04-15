4985e54b78595243ed67d4d98e6a51346fe45847
felipeall <felipe.all.oliver@gmail.com>
2018-04-15 17:10:21 -0300

    Commit de Imagem
    Commit efetuado.

--- src/main/groovy/com/paraondevou/paraondevou/entity/Percurso.groovy
+++ src/main/groovy/com/paraondevou/paraondevou/entity/Percurso.groovy
@@ -1,7 +1,20 @@
 package com.paraondevou.paraondevou.entity
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+@ToString
+@EqualsAndHashCode(includes = ['id'])
 class Percurso {
+    @Id
+    @GeneratedValue
     Long id
+
     Long numOrdem
     Local localPartida
     Local localDestino
