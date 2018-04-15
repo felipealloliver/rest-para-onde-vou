9720034a67508e09c3a51125e708c8b7bd3dfe10
felipeall <felipe.all.oliver@gmail.com>
2018-04-15 20:55:37 -0300

    Post para inserir novo usuario.

--- src/main/groovy/com/paraondevou/paraondevou/controller/UsuarioController.groovy
+++ src/main/groovy/com/paraondevou/paraondevou/controller/UsuarioController.groovy
@@ -3,9 +3,11 @@ package com.paraondevou.paraondevou.controller
 import com.paraondevou.paraondevou.entity.Usuario
 import com.paraondevou.paraondevou.repository.UsuarioRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -20,15 +22,27 @@ class UsuarioController {
     ResponseEntity listar(@PathVariable("id") Long id) {
         Optional<Usuario> usuarioBD = usuarioRepository.findById(id)
 
-        if (usuarioBD.empty()) {
-            ResponseEntity.notFound().build()
-        } else {
+        if (usuarioBD) {
             ResponseEntity.ok(usuarioDB.get())
+        } else {
+            ResponseEntity.notFound().build()
         }
     }
 
     @GetMapping("/all")
     Usuario listarTudo() {
         List<Usuario> lista = usuarioRepository.findAll()
+    }
+
+    @PostMapping("/")
+    ResponseEntity<Usuario> inserirNovo(@RequestBody Usuario usuario) {
+        def usuarioBD = usuarioRepository.findByEmail(usuario.email)
+
+        if (usuarioBD) {
+            ResponseEntity.status(HttpStatus.FORBIDDEN).body(usuario)
+        } else {
+            usuarioRepository.save(usuario)
+            ResponseEntity.ok(usuario)
+        }
     }
 }
