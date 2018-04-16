9f4fd06224d9299761d82203edbc621cfe133b9c
Felipe Lima <fearlimasi@gmail.com>
2018-04-15 21:09:32 -0300

    Commit Felipe Teste

--- src/main/groovy/com/paraondevou/paraondevou/ParaOndeVouApplication.groovy
+++ src/main/groovy/com/paraondevou/paraondevou/ParaOndeVouApplication.groovy
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class ParaOndeVouApplication {
-	
+
 	static void main(String[] args) {
 		SpringApplication.run ParaOndeVouApplication, args
 	}
